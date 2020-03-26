c132813c2adea5bad774d06c07ca92b470eeb224
Matheus Robert <roberttjmelo@hotmail.com>
2020-03-26 17:51:27 -0300

    Be The Hero - sem expo

---
+++ src/pages/Detail/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { View,TouchableOpacity, Image, Text, Linking } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+import email from 'react-native-email'
+import styles from './styles'
+
+import logoImg from '../../assets/logo.png';
+
+export default function Detail(){
+    const navigation = useNavigation()
+    const message = 'Olá APAD, estou entrando em contado pois gostaria de ajudar no caso "Cadelinha atropelada" com valor de R$ 120,00 '
+
+    function navigationBack(){
+        navigation.goBack()
+    }
+    
+    handleEmail = () => {
+        const to = ['matheusroberttjmelo@gmail.com'] // string or array of email addresses
+        email(to, {
+            subject: 'Herói do caso: Cadelinha atropelada',
+            body: message
+        }).catch(console.error)
+    }
+
+    function sendWhatsapp(){
+        Linking.openURL(`whatsapp://send?phone=5582987572380&text=${message}`)
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+
+                <TouchableOpacity onPress={navigationBack}>
+                    <Icon name="arrow-left" size={28} color="#E82041" />
+                </TouchableOpacity>
+                
+            </View>
+            <View style={styles.incident}>
+                    <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG: </Text>
+                    <Text style={styles.incidentValue}>APAD</Text>
+
+                    <Text style={styles.incidentProperty}>CASO: </Text>
+                    <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                    <Text style={styles.incidentProperty}>VALOR: </Text>
+                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+                
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}> 
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action} onPress={handleEmail}> 
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
